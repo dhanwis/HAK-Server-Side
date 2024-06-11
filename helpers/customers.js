594165532b416fc0f096ddc6f0f0b8252318bbca
Muhammed-shamal <muhammedshamalpv@gmail.com>
2024-06-11 10:53:08 +0530

    otp with twilio function added in authentication (login)

--- helpers/customers.js
+++ helpers/customers.js
@@ -1,17 +1,33 @@
+// const twilioClient = require("twilio")(
+//   process.env.TWILIO_ACCOUNT_SID,
+//   process.env.TWILIO_AUTH_TOKEN
+// );
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
 module.exports = {
-    login: (req, res) => {
-      try {
-        const data = req.body; // Access data sent in the request body
-  
-        // Validate and process data
-        console.log(data); // Example: Log received data
-        res.send({ message: "Data received successfully!" }); // Send a response
-      } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: "Internal server error" });
-      }
-    },
-  
-    logout: () => {},
-  };
-  
+  login: async (req, res) => {
+    try {
+      const { phoneNumber } = req.body; // Access data sent in the request body
+
+      client.verify.v2
+        .services("VA5f393ba5eb181ecba3d7ae3dab69a64b")
+        .verifications.create({ to: `+91${phoneNumber}`, channel: "sms" })
+        .then((verification) => console.log(verification.sid))
+        .catch((error) => {
+          console.error("Error sending OTP:", error);
+          res.status(500).json({ error: "Failed to send OTP" });
+        });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: "Internal server error" });
+    }
+  },
+
+  logout: () => {},
+};
